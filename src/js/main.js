9378b9c77749cb50f05364e823f52314f63e0970
elhalili <aminhalili2002@gmail.com>
2022-02-09 13:22:56 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 let http = new XMLHttpRequest();
-http.open('get', 'https://api.alquran.cloud/v1/quran/quran-uthmani');
+http.open('get', 'https://api.alquran.cloud/v1/meta');
 http.onload = function () {
     document.getElementById('onload').style.display = 'none';
 
     var data = JSON.parse(http.response);
-    for(let surah of data.data.surahs) {
+    for(let surah of data.data.surahs.references) {
         document.getElementById('field').innerHTML += `
             <div> ${surah.name} </div>
         `;
