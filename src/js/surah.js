89726072373e6f47b26fefc78fe5ac911476f734
elhalili <aminhalili2002@gmail.com>
2022-02-10 18:14:13 +0100

    final touch

--- src/js/surah.js
+++ src/js/surah.js
@@ -1,13 +1,36 @@
 let number = window.location.search.split("?")[1];
-let test = document.getElementById('test');
+const surahText = document.getElementsByClassName('surah-text')[0];
+const surahName = document.getElementsByTagName('H1')[0];
+const surahAyahNumber = document.getElementsByClassName('info-surah-number')[0];
+const surahPlace = document.getElementsByClassName('info-surah-place')[0];
+
 var data;
+
 const req = new XMLHttpRequest();
 req.open('get', `https://api.alquran.cloud/v1/surah/${number}`);
 req.onload = function () {
-    data = JSON.parse(req.response);
-    for(let ayah of data.data.ayahs) {
-        test.innerHTML += ` ${ayah.text} `;
+    data = JSON.parse(req.response).data;
+    
+    document.getElementById('onload').style.display = 'none';
+    surahName.innerHTML = `${data.name}`
+    document.title = `${data.name}`;
+    surahAyahNumber.innerHTML = `عدد الايات : ${data.numberOfAyahs}`;
+    surahPlace.innerHTML = `مكان النزول : ${data.revelationType}`;
+
+    for(let ayah of data.ayahs) {
+        surahText.innerHTML += `${ayah.text}<span class="number">${ayah.numberInSurah}</span>`;
     }
 }
+req.send();
 
-req.send()
+setInterval(
+    () => {
+        if (document.body.clientHeight == Math.round((window.scrollY + window.screen.height))){
+            document.getElementsByClassName('top')[0].style.display = 'none';
+        }
+        else {
+            document.getElementsByClassName('top')[0].style.display = 'block';
+        }
+    },
+    200
+);
